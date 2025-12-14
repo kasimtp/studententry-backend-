b078365266c769bed85ad8db0913134fe7057296
Muhammed Kasim TP <tpkasimvlr@gmail.com>
2025-12-14 16:29:32 +0530

    new

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,11 +1,10 @@
-import express from 'express'
-import { loginUser, registerUser } from '../controllers/userController.js'
+import express from "express";
+import { loginUser, registerUser } from "../controllers/userController.js";
 
-const userRouter = express.Router()
+const userRouter = express.Router();
 
- registerUser
-userRouter.post('/register',registerUser)
-userRouter.post('/login',loginUser)
+registerUser;
+userRouter.post("/register", registerUser);
+userRouter.post("/login", loginUser);
 
-export default userRouter
-
+export default userRouter;
